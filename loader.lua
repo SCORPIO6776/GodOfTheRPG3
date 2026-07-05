555486fcb2789d5f0141e240b38fb86e9f156524
ScorpioGodOfTheRPG <topila83@gmail.com>
2026-07-05 12:30:58 +0300

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,17 @@
+local encrypted = game:HttpGet("https://raw.githubusercontent.com/SCORPIO6776/GodOfTheRPG3/refs/heads/main/script_encrypted.txt")
+local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+local function dec(data)
+    data = string.gsub(data, '[^'..b..'=]', '')
+    return (data:gsub('.', function(x)
+        if x == '=' then return '' end
+        local r, f = '', (b:find(x) - 1)
+        for i = 6, 1, -1 do r = r .. (f % 2 ^ i - f % 2 ^ (i - 1) > 0 and '1' or '0') end
+        return r
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if #x ~= 8 then return '' end
+        local c = 0
+        for i = 1, 8 do c = c + (x:byte(i) - 48) * 2 ^ (8 - i) end
+        return string.char(c)
+    end))
+end
+loadstring(dec(encrypted))()
